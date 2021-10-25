4cbb60dca1c4b435c9c1dd7c4b5d86485b0a0101
Zachary Burkett <zburkett@splitcubestudios.com>
2021-10-25 13:46:16 -0400

    Update internal/test/issues/issue-illegal_enum_names/doc.go

--- internal/test/issues/issue-illegal_enum_names/doc.go
+++ internal/test/issues/issue-illegal_enum_names/doc.go
@@ -1,3 +1,3 @@
 package illegal_enum_names
 
-//go:generate go run github.com/discord-gophers/goapi-gen/cmd/oapi-codegen  --package=illegal_enum_names -o issue.gen.go spec.yaml
+//go:generate go run github.com/discord-gophers/goapi-gen/cmd/goapi-gen  --package=illegal_enum_names -o issue.gen.go spec.yaml
